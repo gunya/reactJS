fd60200420cb7fe0cb73b7af4162362eb0e707a0
gunya <hepexta@hotmail.com>
2018-11-24 16:17:15 +0300

    Rendering content conditionally 2

--- src/App.js
+++ src/App.js
@@ -36,27 +36,33 @@ class App extends Component {
         backgroundColor: 'white',
         font: 'inherit',
         border: '1x solid blue',
-        paging: '8px'
+        paging: '8px',
+        cursor: 'pointer'
     };
 
-    return (
+      let persons = null;
+
+      if (this.state.showPersons){
+          persons = <div>
+              <Person
+                  name={this.state.persons[0].name}
+                  age={this.state.persons[0].age}/>
+              <Person
+                  name={this.state.persons[1].name}
+                  age={this.state.persons[1].age}
+                  click={this.switchNameHandler.bind(this, "Old Sergei")}
+                  changed={this.nameChangedHandler}>Sergei's wife</Person>
+              <Person
+                  name={this.state.persons[2].name}
+                  age={this.state.persons[2].age}/>
+          </div>;
+      }
+
+      return (
       <div className="App">
-        <h1> Hello there</h1>
-        <button style={style} onClick={() => this.togglePersonsHandler()}>Switch Name</button>
-        { this.state.showPersons ?
-            <div>
-            <Person
-                name={this.state.persons[0].name}
-                age={this.state.persons[0].age}/>
-            <Person
-                name={this.state.persons[1].name}
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, "Old Sergei")}
-                changed={this.nameChangedHandler}>Sergei's wife</Person>
-            <Person
-                name={this.state.persons[2].name}
-                age={this.state.persons[2].age}/>
-        </div> : null}
+          <h1> Hello there</h1>
+          <button style={style} onClick={() => this.togglePersonsHandler()}>Switch Name</button>
+          {persons}
       </div>
     );
   }
