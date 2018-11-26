a8e92f811535284f4af67f8e850639968a0d4b64
gunya <hepexta@hotmail.com>
2018-11-26 23:03:20 +0300

    Radium import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import './App.css';
 import Person from './Person/Person.js';
 
@@ -54,7 +55,11 @@ class App extends Component {
         font: 'inherit',
         border: '1x solid blue',
         paging: '8px',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        ':hover':{
+            backgroundColor: 'lightgreen',
+            color: 'black'
+        }
     };
 
       let persons = null;
@@ -74,6 +79,10 @@ class App extends Component {
               </div>
           );
           style.backgroundColor = "red";
+          style[':hover'] = {
+              backgroundColor: 'salmon',
+              color: 'black'
+          };
       }
 
       const classes = [];
@@ -95,4 +104,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
